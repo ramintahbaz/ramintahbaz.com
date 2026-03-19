9be81f0f8bf2b5c80a4823d811f15efb255e4f3c
ramintahbaz23 <ramin@pattern23.ai>
2026-03-18 21:58:03 -0400

    caching

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,28 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [{ source: '/craft', destination: '/', permanent: false }];
   },
+  async headers() {
+    return [
+      {
+        source: '/videos/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
